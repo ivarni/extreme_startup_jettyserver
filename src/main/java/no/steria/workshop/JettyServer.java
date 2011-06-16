c885b5ea5e38a9b1c252566eda7f04df413ddd9b
Ivar Nilsen <ivn@steria.no>
2011-06-16 18:16:13 +0200

    Jetty starting point

--- src/main/java/no/steria/workshop/JettyServer.java
+++ src/main/java/no/steria/workshop/JettyServer.java
@@ -1,5 +1,20 @@
 package no.steria.workshop;
 
+import java.net.InetAddress;
+
+import org.mortbay.jetty.Server;
+import org.mortbay.jetty.webapp.WebAppContext;
+
 public class JettyServer {
 
+    public static void main(String[] args) throws Exception {
+        Server server = new Server(8086);
+        server.addHandler(new WebAppContext("src/main/webapp", "/"));
+        server.start();
+
+        String ip = InetAddress.getLocalHost().getHostAddress();
+        int port = server.getConnectors()[0].getLocalPort();
+        
+        System.out.println("Started server on http://" + ip + ":" + port + "/");
+    }
 }
---
+++ src/main/java/no/steria/workshop/startup/AnswerServlet.java
@@ -0,0 +1,19 @@
+package no.steria.workshop.startup;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class AnswerServlet extends HttpServlet {
+
+    private static final long serialVersionUID = -5978425016927829406L;
+    
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        System.out.println(req.getParameter("q"));
+    }
+}
